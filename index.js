3759e7a2e7a558731f27d1af762796f2a2878468
mdnajmul <mdnajmulovi@gmail.com>
2022-01-10 14:27:37 +0600

    file upload dependencies added

--- index.js
+++ index.js
@@ -3,25 +3,34 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
-const app = express();
+const fileUpload = require("express-fileupload");
 
+const app = express();
 //PORT
 const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
+// Database Info
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3u7yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 async function run() {
   try {
     await client.connect();
 
+    console.log("database connected successfully");
+
+    //database name
     const database = client.db("luxury_living");
+
+    // Collections
     const servicesCollection = database.collection("services");
   } finally {
     //await client.close();
