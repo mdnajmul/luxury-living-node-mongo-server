c3071238eaa7ef5e8ff4bb43b16685e72b2e211d
mdnajmul <mdnajmulovi@gmail.com>
2022-01-10 15:28:21 +0600

    api create for fetch/display all usesrs from database

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ async function run() {
     });
 
     /* ========================= Service Collection END ======================= */
+
+    /* ========================= User Collection START ======================= */
+
+    // GET - All users
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find({});
+      const users = await cursor.toArray();
+      res.json(users);
+    });
   } finally {
     //await client.close();
   }
